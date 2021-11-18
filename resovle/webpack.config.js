01dab98bd0e3670ced05eedd9e32ec9b5c9b7f67
jiangyeka <940495614@qq.com>
2021-11-18 09:41:34 +0800

    put:[resolve]

---
+++ resovle/webpack.config.js
@@ -0,0 +1,28 @@
+const {resolve}=require('path')
+const HtmlWebpackPlugin =require('html-webpack-plugin')
+module.exports = {
+    entry: './src/js/index.js',
+    output: {
+        filename: "js/[name].js",
+        path: resolve(__dirname,'build'),
+    },
+    module: {
+        rules: [{
+            test: /\.css$/,
+            use:['style-loader','css-loader']
+        }]
+    },
+    plugins: [new HtmlWebpackPlugin() ],
+    mode: "development",
+    // 解析模块
+    resolve: {
+        // 配置解析模块路径别名
+        alias: {
+            $css:resolve(__dirname,'src/css')
+        },
+        // 配置省略文件路径的后缀名
+        extensions: ['.js','.json','.css'],
+        // 告诉webpack解析模块是找那个目录
+        modules: [resolve(__dirname,'../node_modules'),'node_modules']
+    }
+}
