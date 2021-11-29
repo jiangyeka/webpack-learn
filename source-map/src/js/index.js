43f171c8fa21f766bb310b30c0711d695734ed4f
jiangyeka <940495614@qq.com>
2021-11-29 15:22:21 +0800

    put:[oneof&source-map]

---
+++ source-map/src/js/index.js
@@ -0,0 +1,16 @@
+import '../css/iconfont.css'
+import  '../css/index.less'
+import print from "./print";
+function  a(x,y){
+    return x+y;
+}
+
+console.log('awef')()
+print()
+console.log(a(1,2))
+
+if(module.hot){
+    module.hot.accept('./print.js',function (){
+
+    })
+}
---
+++ source-map/src/js/print.js
@@ -0,0 +1,6 @@
+console.log('加载print')
+function print(){
+const a='prinsaaaassaat'
+    console.log(a)
+}
+export default print
